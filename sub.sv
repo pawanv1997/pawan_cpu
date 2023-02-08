e4e26ffda95336994414b95c8b961a8ba60c8d55
Pawan Verma <pawan.v@scaledge.io>
2023-02-08 12:01:46 +0530

    subtraction

--- sub.sv
+++ sub.sv
@@ -4,25 +4,16 @@ module a16bitsub(inp1,inp2,bin,sub,bo);
   input bin;
   output reg [N-1:0]sub;
   output reg bo;
-  wire b11,b12,b13;
-  a4bitsub af0(.inp1(inp1[3:0]),.inp2(inp2[3:0]),.bin(bin),.sub(sub[3:0]),.bo(b11));
-  a4bitsub af1(.inp1(inp1[7:4]),.inp2(inp2[7:4]),.bin(b11),.sub(sub[7:4]),.bo(b12));
-  a4bitsub af2(.inp1(inp1[11:8]),.inp2(inp2[11:8]),.bin(b12),.sub(sub[11:8]),.bo(b13));
-  a4bitsub af3(.inp1(inp1[15:12]),.inp2(inp2[15:12]),.bin(b13),.sub(sub[15:12]),.bo(bo));
-endmodule
-
-//4 bit subtractor
-
-module a4bitsub(inp1,inp2,bin,sub,bo);
-  input [3:0]inp1,inp2;
-  input bin;
-  output reg [3:0]sub;
-  output reg bo;
-  wire b1,b2,b3;
-  fullsubtractor f0(.inp1(inp1[0]),.inp2(inp2[0]),.bin(bin),.sub(sub[0]),.bo(b1));
-  fullsubtractor f1(.inp1(inp1[1]),.inp2(inp2[1]),.bin(b1),.sub(sub[1]),.bo(b2));
-  fullsubtractor f2(.inp1(inp1[2]),.inp2(inp2[2]),.bin(b2),.sub(sub[2]),.bo(b3));
-  fullsubtractor f3(.inp1(inp1[3]),.inp2(inp2[3]),.bin(b3),.sub(sub[3]),.bo(bo));
+  wire [N:0]b;
+  genvar i;
+  generate
+    assign b[0]=bin;
+    for(i=0; i<=N-1; i++)begin
+  fullsubtractor f0(.inp1(inp1[i]),.inp2(inp2[i]),.bin(b[i]),.sub(sub[i]),.bo(b[i+1]));
+      
+    end
+    assign bo=b[N];
+  endgenerate
 endmodule
 //full subtractor
 
